8a843b517cd639febcffe6a1edc1f6092bbca847
DanySarat <102781545+Dany-Sarat@users.noreply.github.com>
2024-11-03 20:22:52 -0600

    agregar logo de umg

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,17 +14,32 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="font-sans text-gray-900 antialiased bg-gray-200 dark:bg-gray-800">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+                
+                <a href="{{ route('dashboard') }}" class="flex justif-center gap-2">
+                        <!--<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> -->
+
+                        <img class="object-scale-down" width="100px" src="{{asset('images/imagen_eorm.png')}}" />
+                        <img class="object-scale-down" width="100px" src="{{asset('images/images.png')}}" />
+
+                    </a>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
+
+        
+        <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between text-center">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">Flowbite™</a>
+      Autor: Danilo Sarat : Ingenieria en sistemas UMG
+    </span>
+
+    </div>
+</footer>
     </body>
 </html>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,12 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('dashboard') }}" class="flex justif-center gap-2">
+                        <!--<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> -->
+
+                        <img class="" width="50px" src="{{asset('images/imagen_eorm.png')}}" />
+                        <img class="" width="50px" src="{{asset('images/images.png')}}" />
+
                     </a>
                 </div>
 
